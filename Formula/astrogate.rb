032a31cd985ec37b697a4965b6bdcdd8136543dd
Astronaute <464180+astronaute77@users.noreply.github.com>
2026-09-13 23:55:48 +0200

    Add astrogate formula

---
+++ Formula/astrogate.rb
@@ -0,0 +1,18 @@
+class Astrogate < Formula
+  desc "Event-driven orchestration of Pi coding sessions in herdr, driven by GitHub"
+  homepage "https://github.com/monadeo/astrogate"
+  url "https://github.com/monadeo/astrogate/releases/download/v0.0.0/astrogate-0.0.0.tar.gz"
+  # url + sha256 are re-stamped by the release workflow on each tag.
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+  depends_on "node"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/astrogate"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/astrogate version")
+  end
+end

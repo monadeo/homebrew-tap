9ea1b0ac44facb862bf4ef7a7c4aeb9e8f6d8b66
Astronaute <464180+astronaute77@users.noreply.github.com>
2026-09-14 00:01:17 +0200

    astrogate 0.0.1

--- Formula/astrogate.rb
+++ Formula/astrogate.rb
@@ -1,9 +1,9 @@
 class Astrogate < Formula
   desc "Event-driven orchestration of Pi coding sessions in herdr, driven by GitHub"
   homepage "https://github.com/monadeo/astrogate"
-  url "https://github.com/monadeo/astrogate/releases/download/v0.0.0/astrogate-0.0.0.tar.gz"
+  url "https://github.com/monadeo/astrogate/releases/download/v0.0.1/astrogate-0.0.1.tar.gz"
   # url + sha256 are re-stamped by the release workflow on each tag.
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "4ddc1095c055bf67de56b6bb5f09b82cf623412a3a53146a18670c41ce3de2fe"
   license "MIT"
   depends_on "node"
 
